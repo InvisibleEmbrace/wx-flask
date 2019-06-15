3228518996be59f2a4726c552300ce8a8b79bbc1
fjw <1281043316@qq.com>
2019-06-16 00:38:35 +0800

    后台管理界面和基本视图跳转

--- application.py
+++ application.py
@@ -6,8 +6,8 @@ import os
 
 
 class Application(Flask):
-    def __init__(self, import_name):
-        super(Application, self).__init__(import_name)
+    def __init__(self, import_name, template_folder=None, root_path=None):
+        super(Application, self).__init__(import_name, template_folder=template_folder, root_path=root_path, static_folder=None)
         self.config.from_pyfile('config/base_setting.py')
         if "ops_config" in os.environ:
             self.config.from_pyfile('config/%s_setting.py' % os.environ['ops_config'])
@@ -16,5 +16,11 @@ class Application(Flask):
 
 
 db = SQLAlchemy()
-app = Application(__name__)
+app = Application(__name__, template_folder=os.getcwd() + "\\web\\templates\\", root_path=os.getcwd())
 manager = Manager(app)
+
+# 注册全局模板函数
+from common.libs.UrlManager import UrlManager
+
+app.add_template_global(UrlManager.buildStaticUrl, 'buildStaticUrl')
+app.add_template_global(UrlManager.buildUrl, 'buildUrl')
