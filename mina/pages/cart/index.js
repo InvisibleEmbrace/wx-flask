63ec11650a6670cdc1b37acae8b67eafbf06dc22
fjw <1281043316@qq.com>
2019-06-28 21:05:10 +0800

    购物车跳转结账

--- mina/pages/cart/index.js
+++ mina/pages/cart/index.js
@@ -3,6 +3,10 @@ var app = getApp();
 Page({
     data: {},
     onLoad: function () {
+        console.log("onload");
+    },
+    onShow: function () {
+        console.log("onShow");
         this.getCartList();
     },
     //每项前面的选中框
@@ -10,7 +14,7 @@ Page({
         var index = e.currentTarget.dataset.index;
         var list = this.data.list;
         if (index !== "" && index != null) {
-            list[ parseInt(index) ].active = !list[ parseInt(index) ].active;
+            list[parseInt(index)].active = !list[parseInt(index)].active;
             this.setPageData(this.getSaveHide(), this.totalPrice(), this.allSelect(), this.noSelect(), list);
         }
     },
@@ -61,6 +65,7 @@ Page({
         var list = that.data.list;
         list[parseInt(index)].number++;
         that.setPageData(that.getSaveHide(), that.totalPrice(), that.allSelect(), that.noSelect(), list);
+        this.setCart(list[parseInt(index)].food_id, list[parseInt(index)].number);
     },
     //减数量
     jianBtnTap: function (e) {
@@ -69,6 +74,7 @@ Page({
         if (list[parseInt(index)].number > 1) {
             list[parseInt(index)].number--;
             this.setPageData(this.getSaveHide(), this.totalPrice(), this.allSelect(), this.noSelect(), list);
+            this.setCart(list[parseInt(index)].food_id, list[parseInt(index)].number);
         }
     },
     //编辑默认全不选
@@ -96,10 +102,10 @@ Page({
         var list = this.data.list;
         var totalPrice = 0.00;
         for (var i = 0; i < list.length; i++) {
-            if ( !list[i].active) {
+            if (!list[i].active) {
                 continue;
             }
-            totalPrice = totalPrice + parseFloat( list[i].price );
+            totalPrice = totalPrice + parseFloat(list[i].price) * list[i].number;
         }
         return totalPrice;
     },
@@ -128,42 +134,100 @@ Page({
     deleteSelected: function () {
         var list = this.data.list;
         var cart_ids = [];
-        list = list.filter(function ( item ) {
-            if( !item.active ){
-                cart_ids.append( item.id );
+        list = list.filter(function (item) {
+            if (!item.active) {
+                cart_ids.append(item.id);
             }
+            return !item.active;
+        });
+        this.setPageData(this.getSaveHide(), this.totalPrice(), this.allSelect(), this.noSelect(), list);
+        //发送请求到后台删除数据
+    },
+    getCartList: function () {
+        var that = this;
+        wx.request({
+            url: app.buildUrl("/cart/index"),
+            header: app.getRequestHeader(),
+            success: function (res) {
+                var resp = res.data;
+                if (resp.code != 200) {
+                    app.alert({"content": resp.msg});
+                    return;
+                }
+                that.setData({
+                    list: resp.data.list,
+                    saveHidden: true,
+                    totalPrice: 0.00,
+                    allSelect: true,
+                    noSelect: false
+                });
+
+                that.setPageData(that.getSaveHide(), that.totalPrice(), that.allSelect(), that.noSelect(), that.data.list);
+            }
+        });
+    },
+    setCart: function (food_id, number) {
+        var that = this;
+        var data = {
+            "id": food_id,
+            "number": number
+        };
+        wx.request({
+            url: app.buildUrl("/cart/set"),
+            header: app.getRequestHeader(),
+            method: 'POST',
+            data: data,
+            success: function (res) {
+            }
+        });
+    },
+    //选中删除的数据
+    deleteSelected: function () {
+        var list = this.data.list;
+        var goods = [];
+        list = list.filter(function ( item ) {
+            if( item.active ){
+                goods.push( {
+                    "id":item.food_id
+                } )
+            }
+
             return !item.active;
         });
         this.setPageData( this.getSaveHide(), this.totalPrice(), this.allSelect(), this.noSelect(), list);
         //发送请求到后台删除数据
-    },
-    getCartList: function () {
-        this.setData({
-            list: [
-                {
-                    "id": 1080,
-					"food_id":"5",
-                    "pic_url": "/images/food.jpg",
-                    "name": "小鸡炖蘑菇-1",
-                    "price": "85.00",
-                    "active": true,
-                    "number": 1
-                },
-                {
-                    "id": 1081,
-					"food_id":"6",
-                    "pic_url": "/images/food.jpg",
-                    "name": "小鸡炖蘑菇-2",
-                    "price": "85.00",
-                    "active": true,
-                    "number": 1
-                }
-            ],
-            saveHidden: true,
-            totalPrice: "85.00",
-            allSelect: true,
-            noSelect: false,
+        wx.request({
+            url: app.buildUrl("/cart/del"),
+            header: app.getRequestHeader(),
+            method: 'POST',
+            data: {
+                goods: JSON.stringify( goods )
+            },
+            success: function (res) {
+            }
         });
-        this.setPageData( this.getSaveHide(), this.totalPrice(), this.allSelect(), this.noSelect(), this.data.list);
-    }
+    },
+     //去结算
+    toPayOrder: function () {
+        var data = {
+            type:"cart",
+            goods: []
+        };
+
+        var list = this.data.list;
+        for (var i = 0; i < list.length; i++) {
+            if ( !list[i].active) {
+                continue;
+            }
+            data['goods'].push({
+                "id": list[i].food_id,
+                "price": list[i].price,
+                "number": list[i].number
+            });
+        }
+
+        wx.navigateTo({
+            url: "/pages/order/index?data=" + JSON.stringify(data)
+        });
+    },
 });
